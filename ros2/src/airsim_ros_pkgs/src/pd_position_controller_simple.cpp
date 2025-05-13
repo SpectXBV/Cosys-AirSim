bb0d390bb778fb85a14f0f0e80a06b736ec81a0c
Alexander James Becoy <a.becoy@spectx.nl>
2025-05-13 17:06:56 +0200

    Change 'odom_local_ned' to 'odom_local'

--- ros2/src/airsim_ros_pkgs/src/pd_position_controller_simple.cpp
+++ ros2/src/airsim_ros_pkgs/src/pd_position_controller_simple.cpp
@@ -76,7 +76,7 @@ void PIDPositionController::initialize_ros()
     airsim_vel_cmd_world_frame_pub_ = nh_->create_publisher<airsim_interfaces::msg::VelCmd>("/airsim_node/" + vehicle_name + "/vel_cmd_world_frame", 1);
 
     // ROS subscribers
-    airsim_odom_sub_ = nh_->create_subscription<nav_msgs::msg::Odometry>("/airsim_node/" + vehicle_name + "/odom_local_ned", 50, std::bind(&PIDPositionController::airsim_odom_cb, this, _1));
+    airsim_odom_sub_ = nh_->create_subscription<nav_msgs::msg::Odometry>("/airsim_node/" + vehicle_name + "/odom_local", 50, std::bind(&PIDPositionController::airsim_odom_cb, this, _1));
     home_geopoint_sub_ = nh_->create_subscription<airsim_interfaces::msg::GPSYaw>("/airsim_node/home_geo_point", 50, std::bind(&PIDPositionController::home_geopoint_cb, this, _1));
     // todo publish this under global nodehandle / "airsim node" and hide it from user
     local_position_goal_srvr_ = nh_->create_service<airsim_interfaces::srv::SetLocalPosition>("/airsim_node/local_position_goal", std::bind(&PIDPositionController::local_position_goal_srv_cb, this, _1, _2));
